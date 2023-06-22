5d013bad9778c75dfdd465ddf68bd765a93c5221
utoppo <utoppo@gmail.com>
2023-06-22 23:04:25 +0200

    fix(Childtheme): load child php files first

--- functions.php
+++ functions.php
@@ -16,10 +16,10 @@ if (!defined('WP_ENV')) {
 // If they aren't, this function redirects the template rendering to use
 // plugin-inactive.php instead and shows a warning in the admin backend.
 if (Init::checkRequiredPlugins()) {
-    FileLoader::loadPhpFiles('inc');
     if (is_child_theme()) {
         FileLoader::loadPhpFiles('inc', [], true);
     }
+    FileLoader::loadPhpFiles('inc');
     add_action('after_setup_theme', ['Flynt\Init', 'initTheme']);
     add_action('after_setup_theme', ['Flynt\Init', 'loadComponents'], 101);
 }
